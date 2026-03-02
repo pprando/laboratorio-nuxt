9a868a8357d4ba4b02d332efb30999947c381807
pprando <pedro.schlens@gmail.com>
2026-03-02 15:30:06 -0300

    fix(useAuth.ts): consertando erro de autenticacao

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -1,10 +1,10 @@
-
 export const useAuth = () => {
   const user = useState('user', () => {
-    if(ProcessingInstruction.client) {
-        const saved = localStorage.getItem('user')
-        return saved ? JSON.parse(saved) : null
-    } return null
+    if (process.client) {
+      const saved = localStorage.getItem('user')
+      return saved ? JSON.parse(saved) : null
+    }
+    return null
   })
 
   const login = (name: string) => {

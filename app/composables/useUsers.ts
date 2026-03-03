1afd57e2ae6f5b78fe57c96258e8d5d7e9869e3b
pprando <pedro.schlens@gmail.com>
2026-03-03 16:28:42 -0300

    feat(users): aprimora o gerenciamento de usuários com funcionalidades de criação e exclusão

--- app/composables/useUsers.ts
+++ app/composables/useUsers.ts
@@ -1,12 +1,43 @@
 export const useUsers = () => {
-    const users = ref([])
-    const loading = ref(false)
+  const users = ref([])
+  const loading = ref(false)
+  const error = ref(null)
 
-    const fetchUsers = async () => {
-        loading.value = true
-        users.value = await $fetch('http://jsonplaceholder.typicode.com/users')
-        loading.value = false
+  const fetchUsers = async () => {
+    try {
+      loading.value = true
+      error.value = null
+      users.value = await $fetch('https://jsonplaceholder.typicode.com/users')
+    } catch (err: any) {
+      error.value = err.message
+    } finally {
+      loading.value = false
     }
+  }
 
-    return { users, loading, fetchUsers }
+  const createUser = async (name: string) => {
+    const newUser = await $fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      body: { name }
+    })
+
+    users.value.push(newUser)
+  }
+
+  const deleteUser = async (id: number) => {
+    await $fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: 'DELETE'
+    })
+
+    users.value = users.value.filter(u => u.id !== id)
+  }
+
+  return {
+    users,
+    loading,
+    error,
+    fetchUsers,
+    createUser,
+    deleteUser
+  }
 }
--- app/pages/users.vue
+++ app/pages/users.vue
@@ -1,16 +1,28 @@
-<script setup>
-const { users, loading, fetchUsers } = useUsers ()
+<script setup lang="ts">
+const { users, loading, error, fetchUsers, createUser, deleteUser } = useUsers()
+
+const name = ref('')
 
 onMounted(fetchUsers)
 </script>
 
 <template>
-<div>
-  <p v-if="loading">Carregando...</p>
-  <ul>
-    <li v-for="user in users" :key="user.id">
-      {{ user.name }}
-    </li>
-  </ul>
-</div>
+  <div>
+    <h1>Users</h1>
+
+    <div>
+      <input v-model="name" placeholder="Novo Usuario" />
+      <button @click="createUser(name)">Criar</button>
+    </div>
+
+    <p v-if="loading">Carregando...</p>
+    <p v-if="loading">{{ error }}</p>
+
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+        <button @click="deleteUser">X</button>
+      </li>
+    </ul>
+  </div>
 </template>
